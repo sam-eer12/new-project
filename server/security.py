1e55e44013344241c12ae69f1a357641f602e6d2
Sameer Gupta <sameer.gupta.ug24@nsut.ac.in>
2025-12-16 17:53:53 +0530

    Update security.py

--- server/security.py
+++ server/security.py
@@ -20,3 +20,10 @@ def create_access_token(data: dict):
     to_encode = data.copy()
     encoded_jwt = jwt.encode(to_encode, os.getenv("SECRET_KEY"), algorithm="HS256")
     return encoded_jwt
+
+def verify_token(token: str):
+    try:
+        payload = jwt.decode(token, os.getenv("SECRET_KEY"), algorithms=["HS256"])
+        return payload
+    except jwt.ExpiredSignatureError:
+        return None
