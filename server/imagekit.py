7e33e11b082c6486fc33cdb93b48a29e30208dc8
Sameer Gupta <sameer.gupta.ug24@nsut.ac.in>
2025-12-28 14:59:41 +0530

    pp

---
+++ server/imagekit.py
@@ -0,0 +1,13 @@
+from imagekitio import ImageKit
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+imagekit = ImageKit(
+    public_key=os.getenv('IMAGEKIT_PUBLIC_KEY'),
+    private_key=os.getenv('IMAGEKIT_PRIVATE_KEY'),
+    url_endpoint=os.getenv('IMAGEKIT_URL_ENDPOINT')
+)
+
+imagekit.create_folder(folder_name='crops', parent_folder_path='/')
